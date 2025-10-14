d7a5315db8e38e718f3dd36d1b5836b0ccf6c5a3
Chirag Bhadani <167410411+Chirag0621@users.noreply.github.com>
2025-10-14 12:52:35 +0000

    Add datatypes example in 01_basics

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all Js code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+let name="hitesh"
+
+let age=18
+let isLogged=false
+
+//number=> 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value 
+//undefinded => 
+// symbol => unique
+
+//object
+
+console.log(typeof "Arnav");
+console.log(typeof age);
+console.log(typeof null);
+
