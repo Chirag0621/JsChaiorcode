270dd2e56308264185eb77281e4176819be28a40
Chirag Bhadani <167410411+Chirag0621@users.noreply.github.com>
2025-10-15 20:11:39 +0000

    Add

---
+++ 01_basics/03_Operation.js
@@ -0,0 +1,18 @@
+// **********************************OPERATION*****************************************************
+
+// let value=3;
+// let negative = -value;
+// console.log(negative);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+
+let str1="hello"
+let str2=" hitesh"
+
+let str3=str1+str2;
+console.log(str3);
+
+console.log(true);
